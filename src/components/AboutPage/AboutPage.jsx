8ed60eff0b6751f808b04099864a6838a86160b5
Jackie Spiess <Jackiespiess22@gmail.com>
2022-01-12 20:08:05 -0600

    test 2

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Form} from 'react-bootstrap'
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -6,12 +7,18 @@ import React from 'react';
 // or even care what the redux state is'
 
 function AboutPage() {
+
+  useEffect(()=>{
+    dispatchEvent( {type: 'FETCH_QUIZ'})
+  })
+
   return (
     <div className="container">
       <div>
         <p>This about page is for anyone to read!</p>
-        <h1>{JSON.stringify('hello world')}</h1>
+        <h1>{JSON.stringify(dispatchEvent)}</h1>
       </div>
+      
     </div>
   );
 }
