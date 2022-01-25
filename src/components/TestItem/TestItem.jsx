af800ec919aeb1c4c0dd7adc4deb36254820f595
Jackie Spiess <Jackiespiess22@gmail.com>
2022-01-25 15:48:26 -0600

    fields available to add test

--- src/components/TestItem/TestItem.jsx
+++ src/components/TestItem/TestItem.jsx
@@ -3,18 +3,35 @@ import {useDispatch, useSelector} from 'react-redux';
 import { Link } from 'react-router-dom';
 
 
+
 function TestItem(props) {
 
   const store = useSelector(store => store);
-  const test = props.test;
+  const [test, setTest] = useState(props.test);
+  
   const dispatch = useDispatch();
-
   const [heading, setHeading] = useState('Functional Component');
 
+  //Amanda 
   const setSelectedTest = (_test) => {
     console.log('clicked edit test');
     dispatch({ type: 'SET_SELECTED_TEST', payload: _test });
   }
+
+const newTest = { 
+       title: '', 
+       points_possible: '', 
+       test_time_limit: '', 
+       question_shuffle: '', 
+       test_attempt_limit: '',
+      //  created_by: user.id, //this is the proctor's id, should be already there in the store 
+     } 
+
+
+const addTest = (event) => {
+  console.log('add test here')
+  dispatch({ type: 'ADD_TEST', payload: { test: newTest } });
+}
 
   return (
     <div>
@@ -26,6 +43,40 @@ function TestItem(props) {
         <button>Edit this test</button>
       </Link>
 
+
+      <div className='add-test'>
+      <h2>Add A Test</h2>
+       <form>
+
+      <div className='input-text'>
+      <label>Title</label> 
+      <input required defaultValue={newTest.title} type="text" name="title" />
+      </div>
+
+       <div className='input-text'>
+       <label>Points Possible</label> 
+       <input required defaultValue={newTest.points_possible}  type="integer" name="points" />
+       </div>
+
+       <div className='input-text'>
+       <label>Test Time Limit</label> 
+       <input required defaultValue={newTest.test_time_limit} type="integer" name="time limit" />
+       </div>
+
+       <div className='input-text'>
+       <label>Question Shuffle</label> 
+       <input required defaultValue={newTest.question_shuffle}  type="boolean" name="true false" />
+       </div>
+
+       <div className='input-text'>
+       <label>Number of Attempts Allowed</label> 
+       <input required defaultValue={newTest.test_attempt_limit}  type="integer" name="attempts" />
+       </div>
+       
+       <button onClick={addTest} variant='contained' color='primary' type='submit'>Add Item</button>
+
+       </form>
+    </div>
     </div>
   );
 }
