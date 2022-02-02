d6f267ec6f010efefdad698dc23918baa8e67d8f
Jackie Spiess <Jackiespiess22@gmail.com>
2022-02-02 08:55:21 -0600

    font and login page

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
+import { Container, Paper } from '@material-ui/core';
+import KyrosLogo from "../../images/KyrosLogo.png"; 
+import "./LoginPage.css"; 
 
 function LoginPage() {
   const history = useHistory();
 
   return (
-    <div>
+    <Paper className="logInPageContainer">
+      <img alt="logo" className="kyros-logo" src={KyrosLogo}/>
+      <h1 className="login-h1">Welcome Back</h1>
+      <p className="login-p">
+        Please sign in to get started!
+      </p>
       <LoginForm />
-
-      <center>
-        <button
+      Not a registered user?
+      <br /> 
+        <a
           type="button"
           className="btn btn_asLink"
           onClick={() => {
@@ -18,9 +26,8 @@ function LoginPage() {
           }}
         >
           Register
-        </button>
-      </center>
-    </div>
+        </a>
+      </Paper>
   );
 }
 
