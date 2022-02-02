01c966a7a1724d3ff5f5674276fb3632f5ce2782
Jackie Spiess <Jackiespiess22@gmail.com>
2022-02-02 12:01:49 -0600

    test item MUI with time stamp display

--- src/components/TestItem/TestItem.jsx
+++ src/components/TestItem/TestItem.jsx
@@ -1,18 +1,28 @@
 import React, { useState } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import { Link } from 'react-router-dom';
+import {useHistory} from "react-router-dom"; 
+import testHeader from "../../images/testHeader.png"; 
+import "../TestItem/TestItem.css"; 
+import SimpleDateTime  from 'react-simple-timestamp-to-date';
 
 //Material-UI imports 
-import Box from '@mui/material/Box';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import TodayIcon from '@mui/icons-material/Today';
+import IconButton from '@mui/material/IconButton';
 
 
 function TestItem(props) {
 
   const store = useSelector(store => store);
   const [test, setTest] = useState(props.test);
-  
+  const history = useHistory(); 
+
   const dispatch = useDispatch();
   const [heading, setHeading] = useState('Functional Component');
   
@@ -21,19 +31,45 @@ function TestItem(props) {
   const setSelectedTest = (_test) => {
     console.log('clicked edit test');
     dispatch({ type: 'SET_SELECTED_TEST', payload: _test });
+  //navigate to /testpage 
+    history.push("/test")
   }
 
 
-  return (
-    <div>
-      <h2>I'm a test</h2>
-      <p>stringified test:{JSON.stringify(test)}</p>
 
-      <Link to="/test" onClick={() => setSelectedTest(test)}>
-        {/* when the edit button is clicked, this will move user to the /test page, and set the selectedTest to this one */}
-        <button>Edit this test</button>
-      </Link>
-    </div>
+  return (
+    <Card sx={{maxWidth: 345}} className="testCard" onClick={()=> setSelectedTest(test)}>
+      <CardMedia
+        component="img"
+        className="cardImg"
+        height="140"
+        image={testHeader}
+        /> 
+        
+
+        <CardContent className="testCardBody">
+          <h2 className="testCardName text">{props.test.title}</h2>
+          <div className="cardCreateDate">
+            <SimpleDateTime dateFormat="DMY" dateSeparator="/" timeSeparator=":">{props.test.create_date}</SimpleDateTime>
+          </div>
+        </CardContent>
+        
+        
+        <CardActions disableSpacing>
+          <IconButton aria-label="edit test">
+            <MoreHorizIcon /> 
+          </IconButton>
+        </CardActions>
+    </Card>
+    // <div>
+    //   <h2>I'm a test</h2>
+    //   <p>stringified test:{JSON.stringify(test)}</p>
+
+    //   <Link to="/test" onClick={() => setSelectedTest(test)}>
+    //     {/* when the edit button is clicked, this will move user to the /test page, and set the selectedTest to this one */}
+    //     <button>Edit this test</button>
+    //   </Link>
+    // </div>
     
   );
 }
