1eabfd5ec1865ede1facd86ed6a60f680b397093
Chris Nelson <chrisnelson@chris.local>
2022-02-07 11:47:07 -0600

    adjusted pool.js to accomodate multiple ports

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.use(express.static('build'));
 
 // App Set //
 const PORT = process.env.PORT || 5001;
-const DB_PORT = process.env.DB_PORT || 5433
+const DB_PORT = process.env.DB_PORT || 5432
 
 /** Listen * */
 app.listen(PORT, () => {
