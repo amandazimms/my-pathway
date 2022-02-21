48c1156232167fe9b66de21b5a0477cf9ad37eec
Amanda Zimmerman <amanda.zimms@gmail.com>
2022-02-20 22:38:34 -0600

    added

--- src/components/BeforeYouBeingPage/BeforeYouBeginPage.jsx
+++ src/components/BeforeYouBeingPage/BeforeYouBeginPage.jsx
@@ -50,8 +50,9 @@ function BeforeYouBeginPage(props) {
     <Avatar className="avatar" style={{ backgroundColor: '#1E2A49'}} >
         <FeaturedVideoIcon className="beginIcon" fontSize="large" style={{color: '#7FC1C5'}}/> 
         </Avatar>
-     <h2 className='subheading'>Have a valid photo ID with you</h2>
-         <h3 className='body'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisi turpis at velit libero, enim elementum consectetur luctus. A, proin ornare nulla nunc.</h3>
+     <h2 className='subheading noTopMargin'>Have a valid photo ID with you</h2>
+         <h3 className='body'>A current drivers' license, passport, or state-issued identification card are all acceptable forms of ID. 
+         In the next screen you will be asked to hold your ID up to your webcam to take a photo to help us verify that you're the one taking the test.</h3>
          </Item>
      </Grid> 
 
@@ -60,8 +61,8 @@ function BeforeYouBeginPage(props) {
           <Avatar className="avatar" style={{ backgroundColor: '#1E2A49' }}>
             <RemoveRedEyeIcon className="beginIcon" style={{color: '#7FC1C5'}}/> 
         </Avatar>
-          <h2 className='subheading'>Keep your eyes on the test</h2>
-         <h3 className='body'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisi turpis at velit libero, enim elementum consectetur luctus. A, proin ornare nulla nunc.</h3>
+          <h2 className='subheading noTopMargin'>Keep your eyes on the test</h2>
+         <h3 className='body'>To ensure the integrity of your exam, please avoid looking away from your computer screen.</h3>
          </Item>
      </Grid> 
 
@@ -70,8 +71,8 @@ function BeforeYouBeginPage(props) {
           <Avatar className="avatar" style={{ backgroundColor: '#1E2A49' }}>
             <VolumeOffIcon className="beginIcon" style={{color: '#7FC1C5'}}/> 
         </Avatar>
-          <h2 className='subheading'>Ensure a quiet enviornment</h2>
-         <h3 className='body'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisi turpis at velit libero, enim elementum consectetur luctus. A, proin ornare nulla nunc.</h3>
+          <h2 className='subheading noTopMargin'>Ensure a quiet enviornment</h2>
+         <h3 className='body'>No outside help is allowed. Nearby voices can render your exam invalid.</h3>
      </Item>
      </Grid> 
 
@@ -80,19 +81,19 @@ function BeforeYouBeginPage(props) {
           <Avatar className="avatar" style={{ backgroundColor: '#1E2A49' }}>
             <MicIcon className="beginIcon" style={{color: '#7FC1C5'}}/> 
         </Avatar>
-          <h2 className='subheading'>Turn your microphone on</h2>
-         <h3 className='body'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisi turpis at velit libero, enim elementum consectetur luctus. A, proin ornare nulla nunc.</h3>
+          <h2 className='subheading noTopMargin'>Turn your microphone on</h2>
+         <h3 className='body'>Please make sure your microphone is on and working, and that the volume is at an audible level. 
+         Your browser may require you to allow microphone permissions for this.</h3>
          </Item>
      </Grid> 
-
 
       <Grid item xs={10}>
           <Item>
           <Avatar className="avatar" style={{ backgroundColor: '#1E2A49' }}>
             <FilterNoneOutlinedIcon className="beginIcon" style={{color: '#7FC1C5'}}/> 
         </Avatar>
-          <h2 className='subheading'>Do not open new tabs or aplications</h2>
-         <h3 className='body'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisi turpis at velit libero, enim elementum consectetur luctus. A, proin ornare nulla nunc.</h3>
+          <h2 className='subheading noTopMargin'>Do not open new tabs or aplications</h2>
+         <h3 className='body'>Use of additional tabs and other applications during this exam is prohibited. Be sure not to open any or your exam results may be rejected.</h3>
          </Item>
      </Grid> 
 </Grid> 
